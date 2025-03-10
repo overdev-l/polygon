5660cc8d4b72ca5bb7ebd3ddab7c37e5c7183de3
yongzhi liu <lyz5@meitu.com>
2025-03-10 10:50:53 +0800

    feat: remove unuse

--- src/render/controlMouse/mouse.ts
+++ src/render/controlMouse/mouse.ts
@@ -5,7 +5,6 @@ export function mouseMoving(this: fabric.Canvas, event: fabric.IEvent) {
   const index = target.get('index')
   const polygon = this.getObjects().find((obj) => obj.get('id') === polygonId)
   const newPoints = target.getCenterPoint()
-  console.log(newPoints)
   const polygonPoints = polygon?.get('points')
   if (polygonPoints) {
     polygonPoints[index] = newPoints
